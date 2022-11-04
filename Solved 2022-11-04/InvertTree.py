c1541bd9d490060d87f1625b644f165310df3e89
Andreas Andersson <116072490+AndreasAnderssonProjekt@users.noreply.github.com>
2022-11-04 10:06:36 +0100

    Create InvertTree.py

---
+++ Solved 2022-11-04/InvertTree.py
@@ -0,0 +1,10 @@
+def invertTree(root):
+        if not root:
+            return
+        
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        invertTree(root.left)
+        invertTree(root.right)
+        return root
