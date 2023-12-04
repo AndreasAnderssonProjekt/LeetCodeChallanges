25e93a98ed5b22c65f7b70512472f28b6d754a1a
Andreas Andersson <116072490+AndreasAnderssonProjekt@users.noreply.github.com>
2023-12-04 10:20:52 +0100

    Create Solution2264.java

---
+++ Solved 2023-12-04/Solution2264.java
@@ -0,0 +1,27 @@
+class Solution2264 {
+  //Solution to LeetCode Ex. 2264 https://leetcode.com/problems/largest-3-same-digit-number-in-string/description/?envType=daily-question&envId=2023-12-04
+    public String largestGoodInteger(String num) {
+        int start = -1;
+        int end = -1;
+        char prev = ' ';
+        String largestGoodInt = "";
+        for(int i = 0; i < num.length(); i++){
+            if(start == -1) start = i;
+            if(num.charAt(i) == prev || prev == ' ') end = i;
+            else{
+                start = i;
+                end = -1;
+            }
+            if(end - start == 2 && end != -1){
+                String goodInt = num.substring(start,end+1);
+                if(largestGoodInt != ""){
+                    if(Integer.parseInt(largestGoodInt) < Integer.parseInt(goodInt)) largestGoodInt = goodInt; 
+                }
+                else largestGoodInt = goodInt;
+            }
+            prev = num.charAt(i);
+        }
+        return largestGoodInt;
+    }
+    
+}
