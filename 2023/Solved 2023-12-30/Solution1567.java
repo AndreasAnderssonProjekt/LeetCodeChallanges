ad0f821e22163ecb9858ee0fb9d50d9c1afe77d8
Andreas Andersson <116072490+AndreasAnderssonProjekt@users.noreply.github.com>
2023-12-30 10:49:12 +0100

    Create Solution1567.java

---
+++ 2023/Solved 2023-12-30/Solution1567.java
@@ -0,0 +1,24 @@
+class Solution1567 {
+    public int getMaxLen(int[] nums) {
+        int res = 0;
+        int neg = 0, pos = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                pos = 0;
+                neg = 0;
+            }
+            else if(nums[i] > 0){
+                pos += 1;
+                neg = (neg == 0) ? 0 : neg + 1;
+            }
+            else{
+                int temp = pos;
+                pos = (neg == 0) ? 0 : neg + 1;
+                neg = temp + 1;
+            }
+            res = Math.max(res, pos);
+        }
+        return res;
+    }
+}
